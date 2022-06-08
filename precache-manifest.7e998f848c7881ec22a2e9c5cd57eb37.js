2dec544cf0e59d2e20c7429593583b1d163c2c2d
Denny Rodas <denny.rh182@gmail.com>
2022-06-07 21:11:15 -0400

    Updates

---
+++ precache-manifest.7e998f848c7881ec22a2e9c5cd57eb37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "012bee67d2b983a98db2",
+    "url": "/calc-app/static/css/main.433c879c.chunk.css"
+  },
+  {
+    "revision": "012bee67d2b983a98db2",
+    "url": "/calc-app/static/js/main.012bee67.chunk.js"
+  },
+  {
+    "revision": "f5b4083897a07b3d905e",
+    "url": "/calc-app/static/js/1.f5b40838.chunk.js"
+  },
+  {
+    "revision": "2d3a55c53a35b0662ab6",
+    "url": "/calc-app/static/js/runtime~main.2d3a55c5.js"
+  },
+  {
+    "revision": "62853e02bd031af2e546f55905c2ef1f",
+    "url": "/calc-app/index.html"
+  }
+];
